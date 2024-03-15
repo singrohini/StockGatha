035789a3bf0c66250164696953e80247093a783d
singrohini <rohinisingh.github@gmail.com>
2024-03-15 19:15:39 +0530

    load children inside rootlayout based on selected route

--- src/pages/layout/RootLayout.tsx
+++ src/pages/layout/RootLayout.tsx
@@ -1,14 +1,20 @@
-import { Outlet } from "react-router-dom";
+import React, { useEffect } from "react";
+import { Link, useLocation } from "react-router-dom";
 
 interface RootLayoutProps {
   children: JSX.Element;
 }
 
 function RootLayout({ children }: RootLayoutProps) {
+  const location = useLocation();
+  console.log("location.pathname", location.pathname);
+
   return (
     <>
       <h1>Main Navigation</h1>
-      <Outlet />
+      <Link to="/stockAnalysis">Go to Analysis Page</Link> &nbsp;
+      <Link to="/">Go to Search Page</Link>
+      <main>{children}</main>
     </>
   );
 }
